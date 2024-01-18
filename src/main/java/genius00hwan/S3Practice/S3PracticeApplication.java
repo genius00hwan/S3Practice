cd5d4ffcb0bbbb58a0200b97d67d8cc513b7572b
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-01-18 18:02:21 +0900

    hello human

---
+++ src/main/java/genius00hwan/S3Practice/S3PracticeApplication.java
@@ -0,0 +1,13 @@
+package genius00hwan.S3Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3PracticeApplication.class, args);
+	}
+
+}
